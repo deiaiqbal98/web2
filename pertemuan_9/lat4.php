6ed437a29994f37c5ae22c4ebb7f6c59f538f212
deiaiqbal98 <80049052+deiaiqbal98@users.noreply.github.com>
2021-04-17 10:04:41 +0700

    pertemuan_9

---
+++ pertemuan_9/lat4.php
@@ -0,0 +1,19 @@
+<HTML>
+
+<HEAD>
+    <TITLE> Tanggal </TITLE>
+</HEAD>
+
+<BODY>
+    <font size="3px">
+        <?php
+        date_default_timezone_set('Asia/Jakarta');
+        echo "Sekarang tanggal ";
+        echo date('d-F-Y');
+        echo "<br>dan jam ";
+        echo date('h:i:s A');
+        ?>
+    </font>
+</BODY>
+
+</HTML>
